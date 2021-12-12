976512cbf0005f7b964cd7762cd2d101dd6e4306
Srikanth Sundarrajan <sriksun@hotmail.com>
2021-12-12 19:45:55 +0530

    Adding license header to source code

--- ExpressionEvaluator/Program.cs
+++ ExpressionEvaluator/Program.cs
@@ -1,6 +1,22 @@
-﻿using Newtonsoft.Json.Linq;
+﻿/*
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*     https://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 using System;
-using System.Diagnostics;
 
 namespace ExpressionEvaluator
 {
@@ -8,115 +24,13 @@ namespace ExpressionEvaluator
     {
         static void Main(string[] args)
         {
-            string jsonStr = @"{
-              'channel': {
-                'title': 'Title1',
-                'link': 'http://title1.book.com',
-                'description': 'Title # 1 book.',
-                'item': [
-                  {
-                    'title': 'Title 1 Chapter 1',
-                    'description': 'Chapter 1 of title 1',
-                    'link': 'http://title1.book.com/chapter1',
-                    'categories': [
-                      'tag1',
-                      'tag2'
-                    ]
-                  },
-                  {
-                    'title': 'Title 1 Chapter 2',
-                    'description': 'Chapter 2 of Title 1',
-                    'link': 'http://title1.book.com/chapter1',
-                    'categories': [
-                      'tag3',
-                      'tag4'
-                    ]
-                  }
-                ]
-              }
-            }";
-
-            JObject json = JObject.Parse(jsonStr);
-
             Context context = new Context();
             context.DeclareVariable("a", typeof(int));
-            context.DeclareVariable("obj", typeof(JObject));
-            context.DeclareVariable("suffix", typeof(string));
-            context.ExportType(typeof(Math));
-            context.ExportType(typeof(Int32));
-            context.ExportType(typeof(String));
-            context.ExportType(typeof(BitConverter));
-            context.ExportType(typeof(DateTime));
-            CompiledExpression cExp = new ExpressionCompiler(context).Compile("String.Concat(obj[\"channel\"][\"item\"][0][\"title\"].ToString().Substring(0,8), suffix)");
+            CompiledExpression cExp = new ExpressionCompiler(context).Compile("a+=3");
             ExpressionEvaluator evaluator = new ExpressionEvaluator(cExp);
-            evaluator.SetVariable("a", 5).SetVariable("obj", json).SetVariable("suffix", "");
-            Func<string> v = evaluator.Evaluate<string>();
-            Console.WriteLine(v());
-            Console.WriteLine(v());
-
-            Stopwatch watch = Stopwatch.StartNew();
-            for (int i = 0; i < 1000000; i++)
-            {
-                evaluator.SetVariable("suffix", i.ToString());
-                v();
-            }
-            Console.WriteLine(watch.ElapsedMilliseconds);
-            watch.Restart();
-            for (int i = 0; i < 1000000; i++)
-            {
-                string s = string.Concat(json["channel"]["item"][0]["title"].ToString().Substring(0, 8), i.ToString());
-            }
-            Console.WriteLine(watch.ElapsedMilliseconds);
-
-            context = new Context();
-            context.DeclareVariable("a", typeof(int));
-            context.ExportType(typeof(Math));
-            context.ExportType(typeof(Int32));
-            cExp = new ExpressionCompiler(context).Compile("Int32.Parse(((Int32)Math.Sqrt(a * a * 1.0)).ToString()) == 5");
-            evaluator = new ExpressionEvaluator(cExp);
             evaluator.SetVariable("a", 5);
-            Func<bool> v1 = evaluator.Evaluate<bool>();
-            Console.WriteLine(v1());
-
-            context = new Context();
-            context.ExportType(typeof(DateTime));
-            cExp = new ExpressionCompiler(context).Compile("new DateTime(DateTime.Now.Ticks)");
-            evaluator = new ExpressionEvaluator(cExp);
-            Func<DateTime> v2 = evaluator.Evaluate<DateTime>();
-            Console.WriteLine(v2());
-
-            context = new Context();
-            context.ExportType(typeof(DateTime));
-            cExp = new ExpressionCompiler(context).Compile("new DateTime(DateTime.Now.Ticks)");
-            evaluator = new ExpressionEvaluator(cExp);
-            Func<DateTime> v3 = evaluator.Evaluate<DateTime>();
-            Console.WriteLine(v3());
-
-            context = new Context();
-            context.DeclareVariable("a", typeof(int));
-            cExp = new ExpressionCompiler(context).Compile("a+=3");
-            evaluator = new ExpressionEvaluator(cExp);
-            evaluator.SetVariable("a", 5);
-            Func<int> v4 = evaluator.Evaluate<int>();
-            Console.WriteLine(v4());
-            Console.WriteLine(context.Variables()["a"]);
-
-            context = new Context();
-            context.DeclareVariable("a", typeof(int));
-            cExp = new ExpressionCompiler(context).Compile("a++");
-            evaluator = new ExpressionEvaluator(cExp);
-            evaluator.SetVariable("a", 5);
-            Func<int> v5 = evaluator.Evaluate<int>();
-            Console.WriteLine(v5());
-            Console.WriteLine(context.Variables()["a"]);
-
-            context = new Context();
-            context.DeclareVariable("a", typeof(int));
-            cExp = new ExpressionCompiler(context).Compile("++a");
-            evaluator = new ExpressionEvaluator(cExp);
-            evaluator.SetVariable("a", 5);
-            Func<int> v6 = evaluator.Evaluate<int>();
-            Console.WriteLine(v6());
+            Func<int> func = evaluator.Evaluate<int>();
+            Console.WriteLine(func());
             Console.WriteLine(context.Variables()["a"]);
         }
     }
